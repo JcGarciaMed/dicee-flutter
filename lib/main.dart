3022b34c12ba1b783fffb714f2344d4bca3fd91a
JORGE <jcgarciamed@gmail.com>
2021-08-30 11:15:15 -0500

    using flatbutton

--- lib/main.dart
+++ lib/main.dart
@@ -22,14 +22,18 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: TextButton(
+              onPressed: () {
+                print("Left Button got pressed");
+              },
               child: Image.asset('images/dice1.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: TextButton(
+              onPressed: () {
+                print("Right Button got pressed");
+              },
               child: Image.asset('images/dice1.png'),
             ),
           ),
